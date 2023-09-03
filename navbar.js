4680fb6180baf33fbc7e29f506dd54496aa2deab
Garcia Del Duca <garciadelduca@gmail.com>
2023-09-02 21:09:29 -0300

    entrega

---
+++ navbar.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const NavBar = () => {
+  return (
+    <div className="navbar">
+      {/* Aquí puedes agregar tus elementos de menú, por ejemplo: */}
+      <ul>
+        <li>Inicio</li>
+        <li>Acerca de</li>
+        <li>Contacto</li>
+      </ul>
+    </div>
+  );
+};
+
+export default NavBar;
